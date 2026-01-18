bb223ac38196dcfc43b522a4a32b8b0e5d69ef2e
karenvv2 <202226058_velazquez@tesch.edu.mx>
2026-01-17 21:26:02 -0600

    Delete backend/src/src directory

--- backend/src/src/routes/src/routes/eventos.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.get("/", (req, res) => {
-  res.json({ mensaje: "Lista de eventos funcionando" });
-});
-
-module.exports = router;
