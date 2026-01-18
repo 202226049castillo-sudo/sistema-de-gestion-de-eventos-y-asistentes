679388cab2449293c0de2e24be7a36d0efd91ad4
karenvv2 <202226058_velazquez@tesch.edu.mx>
2026-01-18 00:41:18 -0600

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,13 @@
 import express from "express";
-import eventosRoutes from "./routes/eventos.routes.js";
+import routes from "./routes/index.js";
 
 const app = express();
 
 app.use(express.json());
 
-// rutas
-app.use("/api/eventos", eventosRoutes);
+app.use("/api", routes);
 
-const PORT = 3000;
+const PORT = 4000;
 app.listen(PORT, () => {
   console.log("Servidor funcionando en puerto", PORT);
 });
