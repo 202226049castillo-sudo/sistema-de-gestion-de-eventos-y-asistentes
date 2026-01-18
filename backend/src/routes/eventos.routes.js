dd510a640248abcc039f7f0f84f45a5bf62e01b7
karenvv2 <202226058_velazquez@tesch.edu.mx>
2026-01-18 00:40:05 -0600

    Update eventos.routes.js

--- backend/src/routes/eventos.routes.js
+++ backend/src/routes/eventos.routes.js
@@ -2,9 +2,8 @@ import { Router } from "express";
 
 const router = Router();
 
-// Ruta de prueba
 router.get("/", (req, res) => {
-  res.json({ mensaje: "Ruta de eventos funcionando" });
+  res.json({ mensaje: "Ruta eventos funcionando" });
 });
 
 export default router;
