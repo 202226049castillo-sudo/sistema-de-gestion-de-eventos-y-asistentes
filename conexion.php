fe875116cbbeb980656e7c03b1669807a80b9cf1
Brandon96-lang <202226032_sanchez@tesch.edu.mx>
2026-01-19 22:17:28 -0600

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -3,14 +3,11 @@ header("Content-Type: application/json; charset=UTF-8");
 
 function leerJSON($archivo) {
     if (!file_exists($archivo)) return [];
-
+    $contenido = file_get_contents($archivo);
+    return $contenido ? json_decode($contenido, true) : [];
 }
 
 function guardarJSON($archivo, $data) {
-    file_put_contents($archivo, json_encode($data, JSON_PRETTY_PRINT));
-}
-
-funcion guardarJSON($archivo, $data) {
     file_put_contents($archivo, json_encode($data, JSON_PRETTY_PRINT));
 }
 
